49bd733e3581bccdd023fd6ab186706b21bd9ed6
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-09 12:31:33 -0700

    add topic button

--- src/components/Dash.jsx
+++ src/components/Dash.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Topic from './Topic'
 
 
-export default class Dash extends React.Component{
-    constructor(){
+export default class Dash extends React.Component {
+    constructor() {
         super()
 
         this.state = {
@@ -19,25 +19,49 @@ export default class Dash extends React.Component{
                 id: 3,
                 name: 'node'
             },
-            ]
+            ],
+            topic: {
+                id: 0,
+                name: 'new topic'
+            }
         }
     }
-    render(){
-        return(
+
+
+    addTopic() {
+        const {topics, topic} = this.state
+        topics.push(topic)
+        this.getTopics()
+    }
+
+    getTopics(){
+        this.setState({
+            topics: this.state.topics
+        })
+        console.log(this.state.topics)
+    }
+
+    render() {
+        return (
             <div className="dash">
 
                 {/* Dash.jsx */}
 
 
                 {/* MAP OUT TOPICS... */}
-                {this.state.topics.map(el=> (
-                    
-                    <Topic 
-                    key = {el.id}
-                    topic = {el}
+                {this.state.topics.map(el => (
+
+                    <Topic
+                        key={el.id}
+                        topic={el}
                     />
 
                 ))}
+
+                {/* ADD TOPIC BUTTON */}
+                <button
+                onClick={()=> this.addTopic()}
+                >add topic</button>
 
             </div>
         )
